87c87f4f9a1776d0011113a03f4a0f8211627869
Jesse Byers <jesse.s.byers@gmail.com>
2019-08-26 10:54:18 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,16 +4,18 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  result = num1 - num2
+  result = num2 - num1
   result
 end
 
 def division(num1, num2)
-
+  result = num1 / num2
+  result
 end
 
 def multiplication(num1, num2)
-
+  result = num1 * num2
+  result
 end
 
 def modulo(num1, num2)
